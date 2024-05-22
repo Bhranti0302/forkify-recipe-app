fbe6b39667f1ea4940f2c1f3d78a186aba106e23
Bhranti0302 <bhrantitamboli40@gmail.com>
2024-05-22 21:24:28 +0530

    Intial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'baca13ed-b5f7-4253-8744-eb3b5b151998';
+export const MODAL_CLOSE_SEC = 2.5;
